6bc3887cab0a8d2eab014f600f9925d8364a68d4
Niels Gasper <niels@gasper.id>
2022-09-13 12:36:19 +0200

    added > and exit

--- src/main/java/id/gasper/Application.java
+++ src/main/java/id/gasper/Application.java
@@ -7,8 +7,14 @@ public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in );
         Calculator c = new Calculator();
+        System.out.print("> ");
         while(sc.hasNextLine()) {
-            System.out.println(c.execute( sc.nextLine() ));
+            String line = sc.nextLine();
+            if ( line.equals( "exit" ) ) {
+                break;
+            }
+            System.out.println(c.execute( line ));
+            System.out.print("> ");
         }
     }
 }
